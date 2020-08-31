86198e9ac6b0871fbf466b5b428f86d316999204
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-08-31 15:09:31 -0500

    feat: Solve is_isogram()
    Implemented a solution using a set.
    This returns as soon as a duplicate is found.

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,8 @@
 def is_isogram(string):
-    pass
+    chars = set()
+    for char in str.lower(string).replace(' ', '').replace('-', ''):
+        if char in chars:
+            return False
+        else:
+            chars.add(char)
+    return True
